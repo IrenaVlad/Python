40e2e98a415a16fb3682b5f95f39fbae84f9360d
IrenaVlad <iriscka@bk.ru>
2022-09-24 19:23:50 +0200

    add lesson1

---
+++ Lesson1.py
@@ -0,0 +1,24 @@
+# print('Hello world')
+
+# Ввод и вывод данных
+# print('Введите a')
+#a = input();
+#print('Введите b')
+#b = input();
+#print(a, b)
+#print('{} -- {}'.format(a,b))
+
+#print('Введите a')
+#a = input();
+#print('Введите b')
+#b = input();
+#c = 30
+#print(a, '+', b, '=', c)
+
+print('Введите a')
+a = int(input());
+print('Введите b')
+b = int(input());
+c = 30
+print(a, '+', b, '=', c)
+print('{} + {} = {}'.format(a, b, c))
