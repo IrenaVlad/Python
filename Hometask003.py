cc8188d1173f989289fa487d754ace72021e66a4
IrenaVlad <iriscka@bk.ru>
2022-09-26 22:17:40 +0200

    add Hometask003

---
+++ Hometask003.py
@@ -0,0 +1,10 @@
+x = float(input('Введите значение x: '))
+y = float(input('Введите значение y: '))
+if(x>0 and y>0):
+ print('1')
+elif(x>0 and y<0):
+ print('4')
+elif(x<0 and y<0):
+ print('3')
+else:
+ print('2')
