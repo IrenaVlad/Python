d1d79dfe8f5135607139ae5610521b03a690ad62
IrenaVlad <iriscka@bk.ru>
2022-09-26 22:23:18 +0200

    add Hometask004

---
+++ Hometask004.py
@@ -0,0 +1,9 @@
+number = int(input('Введите номер четверти: '))
+if(number == 1):
+ print('x>0, y>0')
+elif(number == 2):
+ print('x<0, y>0')
+elif(number == 3):
+ print('x<0, y<0')
+elif(number == 4):
+ print('x>0, y<0')
