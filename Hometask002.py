921a57ae95529e39af37220d8028a281fee891c9
IrenaVlad <iriscka@bk.ru>
2022-09-27 21:05:39 +0200

    change Hometask002

--- Hometask002.py
+++ Hometask002.py
@@ -1,25 +1,6 @@
-x = int(input('Введите значение x: '))
-y = int(input('Введите значение y: '))
-z = int(input('Введите значение z: '))
-a = x * y * z
-b = x + y + z
-if(a>0):
-    a = 0
-elif(a<1):
-    a = 1
-if(b>0):
-    b = 1
-elif(b<1):
-    b = 1
-if(a == b):
-    print("Утверждение истинно")
-elif(a !=b):
-    print('Утверждение ложно')
-
-leftSide = not ( x or y or z)
-rigthSide = not x and not y and not z
-result = leftSide == rigthSide
-if(result == True):
-    print('Утверждение истинно')
-else:
-    print('Утверждение ложно')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(not(x or y or z) == (not x and not y and not z))
+            print(x, y, z)
+print('Утверждение истинно для всех значений предикат')
