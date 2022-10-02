d482dfdd2ac1295416191bfb5bc369b2d71b4999
IrenaVlad <iriscka@bk.ru>
2022-10-02 18:21:22 +0200

    add Hometask3(4)

--- Hometask3(3).py
+++ Hometask3(3).py
@@ -7,18 +7,6 @@ a = [1.1, 1.2, 3.1, 5, 10.01]
 b = []
 for i in range(len(a)-1):
     a[i] = a.split('.')
-    c = int(a[1])
+    c = a[1]
     b.append(c)
-maximum = max(b)
-minimum = min(b)
-summa = maximum - minimum
-print(summa)
-
-a = [1.1, 1.2, 3.1, 5, 10.01]
-b = []
-for symbol in range(a):
-    symbol = symbol.split('.')
-    c = symbol[1]
-    b.append(c)
-summa = max(b) - min(b)
-print(summa)
+print(max(b) - min(b))
