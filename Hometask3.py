7f3247479d902f3ce68c967a15ddaebd960516cc
IrenaVlad <iriscka@bk.ru>
2022-10-05 23:52:23 +0200

    change Hometask4

--- Hometask3.py
+++ Hometask3.py
@@ -1,10 +1,6 @@
 '''Задайте последовательность чисел. Напишите программу, которая выведет 
 список неповторяющихся элементов исходной последовательности.'''
 
-
-# return_unique(lst):
-  # return [i for i in lst if lst.count(i)==1]
-
 from random import randint
 n=int(input('Введите число: '))
 list = []
