469899eed24e9880b20e20d85370064c08e0ad8f
IrenaVlad <iriscka@bk.ru>
2022-10-10 00:32:47 +0200

    change Hometask4

--- Hometask4.py
+++ Hometask4.py
@@ -5,10 +5,17 @@
 from random import randint
 
 k = int(input('Введите натуральную степень k: '))
-a = int(randint(0, 100))
-b = int(randint(0, 100))
-c = int(randint(0, 100))
+a = int(randint(1, 100))
+b = int(randint(1, 100))
+c = int(randint(1, 100))
 print(a, b, c)
 with open('file.txt', 'w') as data:
-    data.write()
+    data.write(str(a))
+    data.write('*x^ ')
+    data.write(str(k))
+    data.write(' + ')
+    data.write(str(b))
+    data.write('*x + ')
+    data.write(str(c))
+    data.write(' = 0')
 data.close()
