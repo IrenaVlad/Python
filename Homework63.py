49efb5099fddfdc8e9876645b10cd1a8988860df
IrenaVlad <iriscka@bk.ru>
2022-10-17 10:21:21 +0200

    add Homework63

--- Homework63.py
+++ Homework63.py
@@ -1,22 +1,14 @@
 '''Задайте последовательность чисел. Напишите программу, которая выведет 
-список неповторяющихся элементов исходной последовательности.'''
+список четных элементов исходной последовательности.'''
 
 from random import randint
-n=int(input('Введите число: '))
-#list = []
-#for i in range(n):
-#    list.append(randint(0, 10))
-#print(f'Исходный списоk {list}')
-#list2 = []
-#for i in list:
-#	if list.count(i) < 2:
-#		list2.append(i)
-#print(f'Конечный список {list2}')
+n = int(input('Введите число: '))
 lst = [randint(0, 10) for i in range(n)]
-print(f'Исходный списоk {lst}')
-def f(list):
-    count = list.count[i]
-    if count[i] < 2:
-        return list[i]
+print(f'Исходный список {lst}')
+def f(spisok):
+    if spisok%2 == 0:
+        return True
+    else:
+        return False
 lst2 = filter(f, lst)
-print(lst2)
+print(f'Конечный список: {list(lst2)}')
